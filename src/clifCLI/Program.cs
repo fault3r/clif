c522a77964412876bea9a1d27e99decd74de5c87
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-06-05 22:50:30 +0330

    initial project

---
+++ src/clifCLI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace clifCLI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("clif CLI");
+        }
+    }
+}
---
+++ src/clifDesign/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace clifDesign.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
