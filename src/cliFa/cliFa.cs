1c42226678182f23c90e0d464e5968ed03191885
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-06-07 23:53:56 +0330

    looking for something

---
+++ src/cliFa/cliFa.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace clif
+{
+    public class cliFa
+    {
+        public string Render(string input)
+        {
+            return input;
+        }
+        
+    }
+}
--- src/clif/Program.cs
+++ src/clif/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using clif;
+
+namespace clif_cli
+{
+    public class Program
+    {
+        private static cliFa clif = new();
+
+
+        public static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("app starts...\n");
+            Console.ResetColor();
+         
+                string str = string.Empty;
+                string? line;
+                while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+                {
+                    str += "[B]" + line +"[E]\n";
+                }
+                Console.WriteLine(str);
+                
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\napp stops...");
+            Console.ResetColor();
+        }
+    }
+}
