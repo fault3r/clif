899399c2f866ae46d5bef1630e167534eabb4449
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-06-12 00:33:09 +0330

    add Code

--- src/cliFa/cliFa.cs
+++ src/cliFa/cliFa.cs
@@ -6,27 +6,51 @@ namespace clif
 {
     public class cliFa
     {
-        private string[] Codes = new string[1];
+        private string[] codes = new string[1];
+        
+        private string Encode(string line)
+        {
+            Regex regex = new Regex(@"`(.*?)`", RegexOptions.Compiled);
+            var matches = regex.Matches(line);
+            codes = new string[matches.Count];
+            int i = 0;
+            foreach (Match match in matches)
+            {
+                codes[i] = match.Value;
+                line = line.Replace(codes[i], $"[[[code{i++}]]]");
+            }
+            return line;
+        }
+
+        private string Decode(string line)
+        {
+            Regex regex = new Regex(@"\[\[\[code\d+\]\]\]", RegexOptions.Compiled);
+            var matches = regex.Matches(line);
+            for (int i = 0; i < codes.Length; i++)
+                line = line.Replace(matches[i].Value, codes[i]);
+            return line;
+        }
+
         public string Render(string line)
         {
-            string rendered = line;
-            rendered = Header(rendered);
-            rendered = Styles(rendered);
-            rendered = Blockquote(rendered);
-            // rendered = Code(rendered);
-            return rendered;
+            line = Encode(line);
+            line = Header(line);
+            line = Styles(line);
+            line = Blockquote(line);
+            line = Decode(line);
+            return line;
         }
 
         private string Header(string line)
         {
             string pattern = string.Empty;
-            pattern = @"^###";    // #Header3
+            pattern = @"^###";                   // #Header3
             if (Regex.IsMatch(line, pattern))
                 return Regex.Replace(line, pattern, $"{Backgrounds.Red}{Foregrounds.Black}") + TextFormats.Reset;
-            pattern = @"^##";    //Header2
+            pattern = @"^##";                    //Header2
             if (Regex.IsMatch(line, pattern))
                 return Regex.Replace(line, pattern, $"{Backgrounds.Yellow}{Foregrounds.Black}") + TextFormats.Reset;
-            pattern = @"^#";    //Header1
+            pattern = @"^#";                     //Header1
             if (Regex.IsMatch(line, pattern))
                 return Regex.Replace(line, pattern, $"{Backgrounds.Green}{Foregrounds.Black}") + TextFormats.Reset;
             return line;
@@ -35,31 +59,31 @@ namespace clif
         private string Styles(string line)
         {
             string pattern = string.Empty;
-            pattern = @"\*\*\*(.+?)\*\*\*";    // **Bold & Italic**
+            pattern = @"\*\*\*(.+?)\*\*\*";      // **Bold & Italic**
             if (Regex.IsMatch(line, pattern))
                 line = Regex.Replace(line, pattern, match =>
                     $"{TextFormats.Bold}{TextFormats.Italic}{match.Groups[1].Value}{TextFormats.ItalicOff}{TextFormats.BoldOff}");
-            pattern = @"\*\*(.+?)\*\*";    // **Bold**
+            pattern = @"\*\*(.+?)\*\*";          // **Bold**
             if (Regex.IsMatch(line, pattern))
                 line = Regex.Replace(line, pattern, match =>
                     $"{TextFormats.Bold}{match.Groups[1].Value}{TextFormats.BoldOff}");
-            pattern = @"\*(.+?)\*";    // *Italic*
+            pattern = @"\*(.+?)\*";              // *Italic*
             if (Regex.IsMatch(line, pattern))
                 line = Regex.Replace(line, pattern, match =>
                     $"{TextFormats.Italic}{match.Groups[1].Value}{TextFormats.ItalicOff}");
-            pattern = @"___(.*?)___";    // ___Strike___
+            pattern = @"___(.*?)___";            // ___Strike___
             if (Regex.IsMatch(line, pattern))
                 line = Regex.Replace(line, pattern, match =>
                     $"{TextFormats.Strike}{match.Groups[1].Value}{TextFormats.StrikeOff}");
-            pattern = @"__(.*?)__";    // __Underline__
+            pattern = @"__(.*?)__";              // __Underline__
             if (Regex.IsMatch(line, pattern))
                 line = Regex.Replace(line, pattern, match =>
                     $"{TextFormats.Underline}{match.Groups[1].Value}{TextFormats.UnderlineOff}");
-            pattern = @"_(.*?)_";    // _Dim_
+            pattern = @"_(.*?)_";                // _Dim_
             if (Regex.IsMatch(line, pattern))
                 line = Regex.Replace(line, pattern, match =>
                     $"{TextFormats.Dim}{match.Groups[1].Value}{TextFormats.DimOff}");
-            pattern = @"%(.*?)%";    // %Blink%
+            pattern = @"%(.*?)%";                // %Blink%
             if (Regex.IsMatch(line, pattern))
                 line = Regex.Replace(line, pattern, match =>
                     $"{TextFormats.Blink}{match.Groups[1].Value}{TextFormats.BlinkOff}");
@@ -68,43 +92,12 @@ namespace clif
 
         private string Blockquote(string line)
         {
-            string pattern = @"^>";
+            string pattern = @"^>";              // > Blockquote
             if (Regex.IsMatch(line, pattern))
                 return Regex.Replace(line, pattern,
                     $"{Backgrounds.Cyan} {Backgrounds.Reset}{Backgrounds.White}{Foregrounds.Black}\"") + $"\"{TextFormats.Reset}";
             return line;
         }
 
-
-
-        public string Encode(string line)
-        {
-            line = "this `is` **bold** and `it is not **bold**` finish `fucking` regex";
-            Regex regex = new Regex(@"`(.*?)`", RegexOptions.Compiled);
-            var matches = regex.Matches(line);
-            int count = matches.Count;
-            Codes = new string[count];
-            int i = 0;
-            foreach (Match match in matches)
-            {
-                Codes[i] = match.Value;
-                line = line.Replace(Codes[i], $"[[[code{i++}]]]");
-            }
-
-            line = Decode(line);
-            return line;
-          
-        }
-
-        public string Decode(string line)
-        {
-            Regex regex = new Regex(@"\[\[\[code\d+\]\]\]", RegexOptions.Compiled);
-            var matches = regex.Matches(line);
-            for (int i = 0; i < Codes.Length; i++)
-            {
-                line=line.Replace(matches[i].Value,Codes[i]);
-            }
-            return line;
-        }
     }
 }
--- src/clif/Program.cs
+++ src/clif/Program.cs
@@ -10,28 +10,30 @@ namespace clif_cli
         public static void Main(string[] args)
         {
 
-            Console.WriteLine(clif.Encode("Aa"));
-            // string output = string.Empty;
-            // if (args.Length == 0)
-            // {
-            //     output = clif.Render("#clif Readme");
-            //     Console.WriteLine(output);
-            //     return;
-            // }
-            // if (args.Length == 1 && !string.IsNullOrEmpty(args[0]))
-            // {
-            //     string file = args[0];
-            //     if (File.Exists(file))
-            //     {
-            //         string[]? lines = File.ReadAllLines(file);
-            //         foreach (string line in lines)
-            //             Console.WriteLine(clif.Render(line));
-            //     }
-            //     else
-            //         Console.WriteLine(clif.Render("###File not found!"));
-            // }
-            // else
-            //     Console.WriteLine(clif.Render("##Invalid command!"));
+            
+            string output = string.Empty;
+            if (args.Length == 0)
+            {
+                //     output = clif.Render("#clif Readme");
+                //     Console.WriteLine(output);
+                //     return;
+                // }
+                // if (args.Length == 1 && !string.IsNullOrEmpty(args[0]))
+                // {
+                //     string file = args[0];
+
+                string file = @"/home/hamed-damavandi/Documents/clif/test.txt";
+                if (File.Exists(file))
+                {
+                    string[]? lines = File.ReadAllLines(file);
+                    foreach (string line in lines)
+                        Console.WriteLine(clif.Render(line));
+                }
+                else
+                    Console.WriteLine(clif.Render("###File not found!"));
+            }
+            else
+                Console.WriteLine(clif.Render("##Invalid command!"));
         }
     }
 }
